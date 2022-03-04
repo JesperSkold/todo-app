4efcc89ad814a9cbdcf268923283b20ef94077bd
Jesper <jesper.olsson@iths.se>
2022-03-04 11:18:10 +0100

    Click outside modal now closes the modal

--- src/components/TodoMenu.vue
+++ src/components/TodoMenu.vue
@@ -20,7 +20,7 @@
 					<img src="../assets/delete_forever.svg" alt="eye" />
 				</p>
 			</div>
-			<div class="fade-layer"></div>
+			<div class="fade-layer" @click="showMenus = false"></div>
 		</div>
 		<span class="material-icons" @click="showMenus = !showMenus"> menu </span>
 	</div>
@@ -32,7 +32,6 @@ export default {
 		return {
 			hideChecks: false,
 			showMenus: false,
-			showMenu: false,
 		};
 	},
 };
@@ -42,6 +41,7 @@ export default {
 .fade-layer {
 	position: fixed;
 	left: 0;
+	cursor: pointer;
 	height: 100vh;
 	width: 100vw;
 	background: rgba(128, 128, 128, 0.486);
